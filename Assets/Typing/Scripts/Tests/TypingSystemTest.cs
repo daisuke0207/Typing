e02a55f6d879b7e399a48db1a6fe42a9e0f4533e
daisuke0207 <daisuke02074869@gmail.com>
2022-08-05 14:42:27 +0900

    fix setting and directory structure

---
+++ Assets/Typing/Scripts/Tests/TypingSystemTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TypingSystem
+{
+    [Test]
+    public void TypingSystemSimplePasses()
+    {
+        Assert.That(true);
+    }
+}
